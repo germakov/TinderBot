26128923646adea927960fdc5a6a9faab514bb1a
German <gvermakov@yandex.ru>
2025-05-26 22:00:20 +0300

    add .env file

--- bot.py
+++ bot.py
@@ -4,7 +4,32 @@ from gpt import *
 from util import *
 
 # тут будем писать наш код :)
+async def start(update, context):
+    text = load_message("main")
+    await send_photo(update, context, "main")
+    await send_text(update, context, text)
 
+async def hello(update, context):
+    user_message = update.message.text
+    await send_text(update, context, "Привет!")
+    await send_text(update, context, "Как дела?")
+    await send_text(update, context, "Вы написали " + user_message)
 
-app = ApplicationBuilder().token("telegram-token").build()
+    await send_photo(update, context, "avatar_main")
+    await send_text_buttons(update, context, "Запустить процесс.", {
+        "start": "Запустить",
+        "stop": "Остановить"
+    })
+
+async def hello_button(update, context):
+    query = update.callback_query.data
+    if query == "start":
+        await send_text(update,context, "Процесс запущен.")
+    else:
+        await send_text(update, context, "Процесс остановлен.")
+
+app = ApplicationBuilder().token("8031207381:AAE2YUJJEYQPoGiG9bJICktas_cwVKhrThE").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
